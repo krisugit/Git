0e2c9ae5293a7b81572389bc656950ac0f731534
krisugit <krycha2524@wp.pl>
2023-11-01 00:08:34 +0100

    Initial Commit

---
+++ Git/GitApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitApp.swift
+//  Git
+//
+//  Created by Krystian Kotus on 31/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct GitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
